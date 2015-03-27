17615b55dd15239d43bf2603ca7258804daad7c0
root <root@localhost.localdomain>
2015-03-27 07:50:45 -0400

    THis is the tool

---
+++ Autocrawler.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Autocrawler For Kali and other os 
+# By : Alexcerus HR
+again='y'
+while  [ $again == 'y' ] || [ $again == 'Y' ];
+do
+clear
+
+#Dont add anything under this code
+cyan='\e[0;36m'
+blue='\e[0;34m'
+grnlight='\e[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\E[31m'
+u='\033[4m'							
+yellow='\e[1;33m'
+endc='\E[0m'							
+version='0.2v'
+cat<<"EOT"
+((
+\\``.
+\_`.``-. 
+( `.`.` `._  
+ `._`-.    `._ 
+   \`--.   ,' `. 
+    `--._  `.  .`. 
+     `--.--- `. ` `. 
+         `.--  `;  .`._ 
+           :-   :   ;. `.__,.,__ __ 
+            `\  :       ,-(     ';o`>.
+              `-.`:   ,'   `._ .:  (,-`,
+                 \    ;      ;.  ,: 
+             ,"`-._>-:        ;,'  `---.,---.
+             `>'"  "-`       ,'   "":::::".. `-.
+              `;"'_,  (\`\ _ `:::::::::::'"     `---.
+              `-(_,' -'),)\`.       _      .::::"'  `----._,-"")
+                   \_,': `.-' `-----' `--;-.   `.   ``.`--.____/ 
+                     `-^--'                \(-.  `.``-.`-=:-.__)
+                                            `  `.`.`._`.-._`--.)
+                                                 `-^---^--.`--
+EOT
+echo -e "$yellow- Autocrawler $version -"
+echo -e $grnlight$u"Available options:"$endc
+echo "fcrawl => File Crawler" #goofile tools
+echo "harvst => The Harvester" 
+echo "dcrawl => Directory CraWler" 
+echo "scrawl => Domain Crawler"
+echo "read   => Readme file" 
+echo "exit   => Exit"
+echo ""
+echo -n -e $cyan$u' Crawler~~: > '$endc; tput sgr0 #insert your choice
+read pass
+if test $pass == 'fcrawl'
+ then
+ echo -n -e $red" [!] set target ~> "; tput sgr0
+ read RHOST
+ echo -n -e $red" [!] set Filetype [txt,pdf,doc] >> "; tput sgr0
+ read filetype
+ goofile -d $RHOST -f $filetype
+ 
+elif test $pass == 'harvst'
+ then
+ echo -n -e $red" [!] set target [target.com] >> "; tput sgr0
+ read domain
+ theharvester -d $domain -l 50 -b google
+
+elif test $pass == 'dcrawl'
+	then
+	echo -n -e $red" [!] set URL [http://url/directory/] >> "; tput sgr0
+	read domain
+	dirb "$domain"
+
+elif test $pass == 'scrawl'
+	 then
+	 echo -n -e $red" [!] set domain [google.com] >> "; tput sgr0
+	 read passlahc1
+	 urlcrazy -p $passlahc1
+elif test $pass == 'read'
+ then
+ cat Readme.txt
+ 
+elif test $pass == 'exit'
+ then
+echo -e $red'Bye'
+      exit
+fi
+
+echo
+echo -e -n "Return to Main ? (y/n) :";
+read again;
+
+    while  [ $again != 'y' ] && [ $again != 'Y' ] && [ $again != 'n' ] && [ $again != 'N' ];
+    do
+       echo -n "Return to main ? (y/n) :";
+       read again;
+    done
+done
+
---
+++ Readme.txt
@@ -0,0 +1,16 @@
+Autocrawler v1.0 : 
+==============
+
+By : Alexcerus HR
+
+  This just easy tool For crawling and get emails from targeted websites and more ...
+- Easy Guide (Installation tools Requirement for running the script without any error)
+- goofile - Use this tool to search for a specific file type in a given domain
+- Tool link : https://code.google.com/p/goofile/
+- TheHarvester - https://code.google.com/p/theharvester/
+- dirb - http://tools.kali.org/web-applications/dirb
+- URL Crazy - http://tools.kali.org/information-gathering/urlcrazy
+~ Team Unknowns ~
+
+
+
